60a2ce9bafe97eac98b4b50a117f6938af35bebd
Oleg Gashnikov <oleg.gashnikov@progresspoint.ru>
2018-02-20 12:52:35 +0300

    Implementing data processing. Adding statistic service

--- ex.py
+++ ex.py
@@ -1,77 +1,160 @@
 # encoding: utf-8
-from flask import Flask
+from flask import Flask, request
 import json
 import random
-from openpyxl import load_workbook
 import sys
 import pandas as pd
+import numpy as np
 
 # set encoding
-#reload(sys)
-#sys.setdefaultencoding("utf-8")
+stdout = sys.stdout
+reload(sys)
+sys.setdefaultencoding('utf-8')
+sys.stdout = stdout
 
-# open data book
-wb = load_workbook('data.xlsx', read_only=True)
 
-# read regions list
-ws = wb.get_sheet_by_name("Регионы")
-rg_codes = [r[0].value for r in ws.iter_rows()]
-rg_codes = rg_codes[1:]
 
-# read macroregions
-ws = wb.get_sheet_by_name("Макрорегионы")
-mrg_codes = [r[0].value for r in ws.iter_rows()]
-mrg_codes = mrg_codes[1:]
+# returns color codes
+def get_color(plan, fact):
+	if fact > (plan - plan / 10.0):
+		return 'G'
+	if fact > (plan - plan / 4):
+		return 'Y'
+	return 'R'
 
 # Flask
 app = Flask(__name__)
 
-# returns color codes
-def get_color(plan, fact):
-	if abs(plan - fact) < 20:
-		return 'Y'
-	if plan > fact:
-		return 'R'
-	return 'G'
+# load book
+xl = pd.ExcelFile("data.xlsx")
 
-# create dummy object
-LO = 50
-HI = 150
+# parse shit
+df = xl.parse("Лист1")
 
-def create_dummy():
-	regions = []
-	for i in rg_codes:
-		plan = random.randint(LO, HI)
-		fact = random.randint(LO, HI)
-		regions.append(
-				{
-				'id': i,
-				'plan': plan,
-				'fact': fact,
-				'color': get_color(plan, fact)
-				}
-			)
-	mregions = []
-	for i in mrg_codes:
-		plan = random.randint(LO, HI)
-		fact = random.randint(LO, HI)
-		mregions.append(
-				{
-				'id': i,
-				'plan': plan,
-				'fact': fact,
-				'color': get_color(plan, fact)
-				}
-			)
-	return {
-		'regions': regions,
-		'macroregions': mregions
-	}
-
+print 'debug url for region data:'
+print 'http://127.0.0.1:5000/biserver/api/v1.0/regions?services[]=1&services[]=2&channels[]=2&channels[]=5&year=2017&months[]=3&months[]=4'
+print
+print 'debug url for statistic data:'
+print 'http://127.0.0.1:5000/biserver/api/v1.0/statistic?year1=2017&year2=2016&months1[]=1&months1[]=2&months2[]=1&months2[]=2&mregion=100&region=54'
 
 
 
 @app.route('/biserver/api/v1.0/regions', methods=['GET'])
-def hello_world():
-	dummy = create_dummy()
-    return json.dumps(dummy)
+def data():	
+	year = 0
+	monthes = []
+	products = []
+	chanels = []
+	# parse args
+	for a in request.args:
+		print "key={}, value={}".format(a, request.args.getlist(a))
+		if a == 'months[]':
+			for v in request.args.getlist(a):
+				monthes.append(int(v))
+		elif a == 'services[]':
+			for v in request.args.getlist(a):
+				products.append(int(v))
+		elif a == 'channels[]':
+			for v in request.args.getlist(a):
+				chanels.append(int(v))
+		elif a == 'year':
+			year = int(request.args.getlist(a)[0])
+
+	# selection
+	sel = df[(df.y == year) & (df.m.isin(monthes)) & (df.ch_id.isin(chanels)) & (df.DRMS_id.isin(products))]
+
+	# regions aggregation
+	reg = sel.groupby('region_key')
+	reg = reg.agg({'plan_val': np.sum, 'fact_val': np.sum}).fillna(0)
+
+
+	# mrf aggregation
+	mrg = sel.groupby('mrf_key')
+	mrg = mrg.agg({'plan_val': np.sum, 'fact_val': np.sum}).fillna(0)
+
+	# create return data structure
+	regions = []
+	for index, row in reg.iterrows():
+		regions.append(
+			{
+			'id':		index,
+			'plan':		row['plan_val'],
+			'fact':		row['fact_val'],
+			'color':	get_color(row['plan_val'], row['fact_val'])
+			}
+		)
+	mregions = []
+	for index, row in mrg.iterrows():
+		mregions.append(
+			{
+			'id':		index,
+			'plan':		row['plan_val'],
+			'fact':		row['fact_val'],
+			'color':	get_color(row['plan_val'], row['fact_val'])
+			}
+		)
+	d = {
+		'regions':		regions,
+		'macroregions':	mregions
+		}
+
+	return json.dumps(d)
+
+@app.route('/biserver/api/v1.0/statistic', methods=['GET'])
+def stat():
+	year1 = 0
+	year2 = 0
+	monthes1 = []
+	monthes2 = []
+	mregion = 0
+	region = 0
+	# parse args
+	for a in request.args:
+		print "key={}, value={}".format(a, request.args.getlist(a))
+		if a == 'months1[]':
+			for v in request.args.getlist(a):
+				monthes1.append(int(v))
+		elif a == 'months2[]':
+			for v in request.args.getlist(a):
+				monthes2.append(int(v))
+		elif a == 'year1':
+			year1 = int(request.args.getlist(a)[0])
+		elif a == 'year2':
+			year2 = int(request.args.getlist(a)[0])
+		elif a == 'mregion':
+			mregion = int(request.args.getlist(a)[0])
+		elif a == 'region':
+			region = int(request.args.getlist(a)[0])
+
+	# selection
+	if region > 0:
+		prd1 = df[(df.y == year1) & (df.m.isin(monthes1)) & (df.mrf_key == mregion) & (df.region_key == region)]
+		prd2 = df[(df.y == year2) & (df.m.isin(monthes2)) & (df.mrf_key == mregion) & (df.region_key == region)]
+	else:
+		prd1 = df[(df.y == year1) & (df.m.isin(monthes1)) & (df.mrf_key == mregion)]
+		prd2 = df[(df.y == year2) & (df.m.isin(monthes2)) & (df.mrf_key == mregion)]
+
+	# agregation
+	grouped1 = prd1.groupby('DRMS_id')
+	a1 = grouped1.agg({'plan_val': np.sum, 'fact_val': np.sum}).fillna(0)
+	grouped2 = prd2.groupby('DRMS_id')
+	a2 = grouped2.agg({'plan_val': np.sum, 'fact_val': np.sum}).fillna(0)
+
+	# join periods
+	p1p2 = pd.merge(a1, a2, left_index=True, right_index=True, how='outer', suffixes=('_1', '_2'))
+
+	# create return data object
+	d = []
+	for index, row in p1p2.iterrows():
+		d.append(
+			{
+			'product_id'		: index,
+			'period1_plan_sum'	: row['plan_val_1'],
+			'period2_plan_sum'	: row['plan_val_2'],
+			'period1_fact_sum'	: row['fact_val_1'],
+			'period2_fact_sum'	: row['fact_val_2']
+			}
+		)
+
+	return json.dumps(d)
+
